c86186d74ac22db512abe97c7baf32ee749a3e97
FloydP1nk <113801739+FloydP1nk@users.noreply.github.com>
2025-09-09 13:20:30 +0300

    add loading port number from env

--- main.go
+++ main.go
@@ -3,9 +3,12 @@ package main
 import (
 	"context"
 	"github.com/gorilla/mux"
+	"github.com/joho/godotenv"
 	httpSwagger "github.com/swaggo/http-swagger"
 	"log"
 	"net/http"
+	"os"
+
 	_ "subscription-service/docs"
 )
 
@@ -15,6 +18,11 @@ import (
 // @host localhost:8080
 // @BasePath /
 func main() {
+
+	err := godotenv.Load()
+	if err != nil {
+		log.Println("⚠️ .env не найден, используем переменные окружения")
+	}
 
 	//БД
 	conn, err := ConnectDB()
@@ -36,7 +44,7 @@ func main() {
 	// Swagger UI доступен по адресу http://localhost:8080/swagger/index.html
 	r.PathPrefix("/swagger/").Handler(httpSwagger.WrapHandler)
 
-	port := "8080"
+	port := os.Getenv("SERVER_PORT")
 	log.Printf("Сервер запущен на http://localhost:%s\n", port)
 	if err := http.ListenAndServe(":"+port, r); err != nil {
 		log.Fatalf("Ошибка при запуске сервера: %v", err)
